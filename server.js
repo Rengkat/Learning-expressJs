a742ebe4561a23917b0dc92baff93a24e6af8d4e
Rengkat <successinmotion11@gmail.com>
2023-12-24 00:38:01 +0100

    put reuest

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Joi = require("joi");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const courses = [
@@ -24,11 +25,41 @@ app.get("/api/courses/:id", (req, res) => {
   }
 });
 app.post("/api/courses", (req, res) => {
+  //   const schema = {
+  //     name: Joi.string().min(3).required(),
+  //   };
+  //   const result = schema.validate(req.body);
+  //   console.log(result);
+  if (!req.body.name || req.body.name.length < 3) {
+    //send res with status 400: bad req
+    res.status(400).send("Please enter a valid name");
+    return;
+  }
   const course = {
     id: courses.length + 1,
-    course: req.body.course,
+    course: req.body.name,
   };
   courses.push(course);
+  res.send(course);
+});
+//put
+app.put("/api/course/:id", (req, res) => {
+  //get the course
+  const course = courses.find((c) => c.id === Number(req.params.id));
+
+  //check if it exist in the courses
+  if (!course) {
+    res.status(400).send("Course not exist");
+  }
+  //validate
+  if (!req.body.name || req.body.name.length < 3) {
+    //send res with status 400: bad req
+    res.status(400).send("Please enter a valid name");
+    return;
+  }
+
+  // update
+  course.name = req.body.name;
   res.send(course);
 });
 app.listen(PORT, () => console.log(`Listing on port ${PORT}`));
